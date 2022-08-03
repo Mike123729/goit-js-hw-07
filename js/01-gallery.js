ddf00452e86af9274e77ac1fce4481b9a25371ac
Mike123729 <konst.nikolas@gmail.com>
2022-08-03 12:22:06 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,7 @@ const item = galleryItems
   .map(
     ({ preview, original, description }) =>
       `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"

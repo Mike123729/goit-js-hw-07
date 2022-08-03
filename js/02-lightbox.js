2e4e80d19bad0d7d32468e3fa79e08eb4b28e8ab
Mike123729 <konst.nikolas@gmail.com>
2022-08-03 14:24:26 +0300

    add second gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const item = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryEl.innerHTML = item;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+  captionPosition: 'bottom',
+  showCounter: false,
+});
